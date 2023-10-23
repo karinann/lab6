0816d9e45a4175b1e9c00ae7f74168da1e77b8ac
MichaelAmiot <michaelamiot@ufl.edu>
2023-10-23 03:28:59 -0400

    made a change to main.

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ def encode(password):
 
 
 def decode(encoded):  # Michael Amiot
-    final = ''
+    decoded = ''
     password_list = list(encoded)
     # iterate through each item in password_list and subtract 3
     for index, item in enumerate(password_list):
         password_list[index] = int(item) - 3
-        final += str(password_list[index])
-    return final
+        decoded += str(password_list[index])
+    return f'The encoded password is {encoded}, and the original password is {decoded}'
 
 
 print("Encoder/Decoder Menu\n--------\n1. Encode\n2. Decode\n3. Quit")
@@ -41,7 +41,7 @@ while True:
         print()
 
     if menu_option == 2:  # Michael Amiot Decode option
-        print(f'The encoded password is {stored}, and the original password is {decode(stored)}')
+        print(decode(stored))
         print("\nEncoder/Decoder Menu\n--------\n1. Encode\n2. Decode\n3. Quit")
         menu_option = int(input("\nSelect a Menu Option: "))
         print()
